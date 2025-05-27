604373bcc96235f2b4f0fed99b9bbf7e6c2fe4a6
Samuele Trapani <samuele.trapani@studio.unibo.it>
2025-05-27 11:51:53 +0200

    added average grade chart

--- App/Learnova/Commons/Grade.cs
+++ App/Learnova/Commons/Grade.cs
@@ -5,6 +5,7 @@ public class Grade
     public string description { get; set; } = "";
     public double value { get; set; } = 0;
     public int id { get; set; }
+    public DateTime date { get; set; }
 
     public override bool Equals(object? obj)
     {
--- App/Learnova/obj/Debug/net9.0/Learnova.AssemblyInfo.cs
+++ App/Learnova/obj/Debug/net9.0/Learnova.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fff751bb166dd4b121a358afa64069a7dce859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f138cc992917a53deddc77219f5d3554c8d33b0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
