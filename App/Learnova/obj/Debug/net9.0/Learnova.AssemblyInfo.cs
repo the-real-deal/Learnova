f7b0a55eba333c2184ad3e901e3ef12e15cc041e
elettra-ven <elettraventura27@gmail.com>
2025-06-06 12:55:40 +0100

    added class and list of students page

--- App/Learnova/obj/Debug/net9.0/Learnova.AssemblyInfo.cs
+++ App/Learnova/obj/Debug/net9.0/Learnova.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50c16af898d37ad584482b5bf67e7082c306c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6751a3ada3697afafba17460a6149110f3074a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learnova")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
